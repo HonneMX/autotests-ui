96aebc1f90350b7553d200951c7f3ec671e8b6ee
Sergey Yaushev <svyaushev@gmail.com>
2025-03-15 19:34:31 +0300

    Фикстуры Pytest.Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright, Page, Playwright
+from playwright.sync_api import sync_playwright, Page, Playwright, expect
 import pytest
 
 
@@ -13,3 +13,34 @@ import pytest
 def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
+
+
+@pytest.fixture(scope="session")
+def initialize_browser_state():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        # Открыть страницу
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+        # Заполнить форму регистрации и нажать на кнопку "Registration"
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill('user@gmail.com')
+        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input.fill('username')
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill('password')
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        # Сохранить состояние браузера
+        context.storage_state(path='browser-state.json')
+
+
+@pytest.fixture(autouse=True)
+def chromium_page_with_state():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state='browser-state.json')
+        yield context.new_page()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,44 +1,25 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect, Page
 import pytest
+
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        # Открыть страницу
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+@pytest.mark.usefixtures(
+    "initialize_browser_state"
+)
+def test_empty_courses_list(chromium_page_with_state: Page):
+    page = chromium_page_with_state
 
-        # Заполнить форму регистрации и нажать на кнопку "Registration"
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user@gmail.com')
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
+    # Открыть страницу "Courses" должна открыться без авторизации
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-        # Сохранить состояние браузера
-        context.storage_state(path='browser-state-playwright-courses.json')
+    # Проверить наличие и текст заголовка "Courses"
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
 
-    # Создать новую сессию браузера. В контекст необходимо подставить сохраненное состояние
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state-playwright-courses.json')
-        page = context.new_page()
-
-        # Открыть страницу "Courses" должна открыться без авторизации
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-
-        # Проверить наличие и текст заголовка "Courses"
-        courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses_title).to_be_visible()
-        expect(courses_title).to_have_text('Courses')
-
-        # Проверить наличие и текст блока "There is no results"
-        result_text_block = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(result_text_block).to_be_visible()
-        expect(result_text_block).to_have_text('There is no results')
+    # Проверить наличие и текст блока "There is no results"
+    result_text_block = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(result_text_block).to_be_visible()
+    expect(result_text_block).to_have_text('There is no results')
+    page.wait_for_timeout(5000)
