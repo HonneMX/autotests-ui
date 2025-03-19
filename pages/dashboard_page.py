2fd2b876ad8249dbe6c27e013a519afa2e012d64
Sergey Yaushev <svyaushev@gmail.com>
2025-03-19 08:29:12 +0300

    Работа над ошибками

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -6,19 +6,19 @@ class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        # Верхнее меню
+
         self.navigation_title_text = page.get_by_test_id('navigation-navbar-app-title-text')
         self.navigation_welcome_title = page.get_by_test_id('navigation-navbar-welcome-title-text')
 
-        # Список слева
+
         self.dashboard_list_item_title = page.get_by_test_id('dashboard-drawer-list-item-title-text').locator('span')
         self.courses_list_item_title = page.get_by_test_id('courses-drawer-list-item-title-text').locator('span')
         self.logout_button = page.get_by_test_id('logout-drawer-list-item-title-text').locator('span')
 
-        # Заголовок в центре
+
         self.dashboard_toolbar_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
-        # Виджеты в центре
+
         self.student_widget_title = page.get_by_test_id('students-widget-title-text')
         self.student_widget_bar_char = page.get_by_test_id('students-bar-chart')
         self.activities_widget_title = page.get_by_test_id('activities-widget-title-text')
@@ -28,6 +28,6 @@ class DashboardPage(BasePage):
         self.scores_widget_title = page.get_by_test_id('scores-widget-title-text')
         self.scores_widget_scatter_char = page.get_by_test_id('scores-scatter-chart')
 
-    def check_page_title(self):
+    def check_dashboard_title_visible(self):
         expect(self.dashboard_toolbar_title).to_be_visible()
-        self.page.wait_for_timeout(5000)
+
--- tests/test_successful_registration.py
+++ tests/test_successful_registration.py
@@ -11,21 +11,4 @@ def test_succesfull_registration(registration_page: RegistrationPage, dashboard_
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
     registration_page.fill_registration_form(email='user@gmail.com', username='username', password='password')
     registration_page.click_registration_button()
-    dashboard_page.check_page_title()
-
-    # chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    #
-    # email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-    # email_input.fill('user@gmail.com')
-    #
-    # username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-    # username_input.fill('username')
-    #
-    # password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-    # password_input.fill('password')
-    #
-    # registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-    # registration_button.click()
-    #
-    # dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-    # expect(dashboard_title).to_be_visible()
+    dashboard_page.check_dashboard_title_visible()
